50461b13897678dbf2640b9e0df29f65957bc1d9
tanvirn2004 <TANVIR.NIBIR@metropolia.fi>
2024-08-30 12:24:09 +0600

    this is my initial commit

---
+++ assignment/assigment 3/leap_year.py
@@ -0,0 +1,11 @@
+print("welcome to leap year testing code")
+year = int(input("Enter a year: "))
+if year % 4 ==0:
+    print("it's Leap Year")
+elif year % 400 == 0 :
+    print("Leap Year")
+
+elif year % 100 ==0 and year % 4 == 0 :
+    print("Leap Year")
+else:
+    print("It's not leap year  Leap Year")
