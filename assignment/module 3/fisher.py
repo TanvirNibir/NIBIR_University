2a624e1141804528c9debf514b8229230b8d899a
tanvirn2004 <TANVIR.NIBIR@metropolia.fi>
2024-09-10 20:49:52 +0300

    t

--- assignment/module 3/fisher.py
+++ assignment/module 3/fisher.py
@@ -2,7 +2,8 @@ size_zander = int(input("Enter the size of zender in cm : "))
 if size_zander >= 42 :
     print("fish released successfully")
 else:
+    below = 42 - size_zander
     print("fish released unsuccessfully")
-    print(f"The fish is {size_zander:.2f} centimeters below the size limit")
+    print(f"The fish is {below:.2f} centimeters below the size limit")
 
 
--- exercises/exercise.py
+++ exercises/exercise.py
@@ -1,38 +1,20 @@
-# import random
-# number_to_guess = random.randint(1, 100)
-# guess = None
-# while guess != number_to_guess:
-#     guess = int(input("Guess a number between 1 and 100: "))
-#     if guess == number_to_guess:
-#         print("You got it right!")
-#     elif guess < number_to_guess:
-#         print("Your guess is too low!")
-#     else:
-#         print("Your guess is too high!")
-#         break
-
-
-# name = ["Tanvir", "Rahman", "Nibir"]
-# for name in name :
-#     print(name)
-# fruits = ["apple", "pineapple", "Banana", "jackfruit"]
-# for fruits in fruits:
-#     print(fruits)
-
-#list
+# lots_in_gram = 13.3
+# pounds_in_talent = 20
+# lots_per_pound = 32
 #
-# name = ["tanvir", "Rahman", "nibir", "payel", "omar", "niloy"]
-# print(name[0])
-# name[2] = "shifat"
-# print(name[2])
-# print(name)
-# name.append("sakib")
-# name.append("sumon")
-# name.append("tahasin")
-# print(name)
-def nibir():
-    print("tanvir")
-    print("tanvir")
-    print("Tanvir " + "Rahman " + "Nibir")
-
-nibir()
+# talents = float(input("Enter talents: "))
+# pounds = float(input("Enter pounds: "))
+# lots = float(input("Enter lots: "))
+# total_lots = (talents * pounds_in_talent * lots_per_pound ) + (pounds * lots_per_pound ) + lots
+# total_grams = total_lots * lots_in_gram
+#
+#
+# kilograms = int(total_grams // 1000)
+# grams = total_grams % 1000
+# print(f"The weight in modern units: {kilograms} kilograms and {grams:.2f} grams.")
+size_zander = int(input("Enter the size of zender in cm : "))
+if size_zander >= 42 :
+    print("fish released successfully")
+else:
+    print("fish released unsuccessfully")
+    print(f"The fish is {size_zander:.2f} centimeters below the size limit")
